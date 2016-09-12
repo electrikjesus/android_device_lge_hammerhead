f89a282bb5ba47e12fbc7b194dd199ca0f69a65f
electrikjesus <electrikjesus@gmail.com>
2016-09-12 10:55:40 -0400

    Create bliss.mk

---
+++ bliss.mk
@@ -0,0 +1,21 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit Bliss vendor
+$(call inherit-product, vendor/bliss/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := bliss_hammerhead
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:7.0/NRD90M/3067468:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 7.0 NRD90M 3067468 release-keys"
